911638faceb47b39bbed4717b82cf38032cd0087
warrenbrown <warren@katapultmedia.com>
2016-08-22 14:57:57 -0500

    added specs

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160206192502) do
+ActiveRecord::Schema.define(version: 20160210032426) do
 
   create_table "comments", force: :cascade do |t|
     t.string   "name"
@@ -23,12 +23,20 @@ ActiveRecord::Schema.define(version: 20160206192502) do
 
   add_index "comments", ["post_id"], name: "index_comments_on_post_id"
 
+  create_table "photos", force: :cascade do |t|
+    t.string   "file"
+    t.integer  "post_id"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  add_index "photos", ["post_id"], name: "index_photos_on_post_id"
+
   create_table "posts", force: :cascade do |t|
     t.string   "title"
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
-    t.string   "attachment"
   end
 
   create_table "users", force: :cascade do |t|
